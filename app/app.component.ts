2cfb5243869f29805db9794478e17a47764e3562
jorgeucano <jorgeucano@gmail.com>
2016-04-23 19:29:37 -0300

    add and exposes hero array

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,19 @@ export class Hero{
   name: String;
 }
 
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
+
 @Component({
     selector: 'my-app',
     template: `
@@ -24,9 +37,6 @@ export class AppComponent {
 
   title = "Tour of Heroes";
 
-  hero: Hero = {
-    id : 1,
-    name: "WindStorm"
-  };
+  public heroes = HEROES;
 
 }
